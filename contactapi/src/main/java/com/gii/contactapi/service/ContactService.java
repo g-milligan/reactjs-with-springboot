49d0acfa0acc9bbeeee70d87ccab39a3ec8b90f9
Gregg Milligan II <milligangregg@gmail.com>
2024-07-06 10:22:36 -0400

    Contact Service - Part 1

---
+++ contactapi/src/main/java/com/gii/contactapi/service/ContactService.java
@@ -0,0 +1,37 @@
+package com.gii.contactapi.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.gii.contactapi.domain.Contact;
+import com.gii.contactapi.repo.ContactRepo;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+@Transactional(rollbackOn = Exception.class)
+@RequiredArgsConstructor
+public class ContactService {
+    private final ContactRepo contactRepo;
+
+    public Page<Contact> getAllContacts(int page, int size) {
+        return contactRepo.findAll(PageRequest.of(page, size, Sort.by("name")));
+    }
+
+    public Contact geContact(String id) {
+        return contactRepo.findById(id).orElseThrow(() -> new RuntimeException("Contact not found"));
+    }
+
+    public Contact createContact(Contact contact) {
+        return contactRepo.save(contact);
+    }
+
+    public void deleteContact(Contact contact) {
+        contactRepo.delete(contact); // assignment to implement delete
+    }
+}
