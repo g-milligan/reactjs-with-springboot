29d7391776af70cd237147f4eacd93fa367d49c1
Gregg Milligan II <milligangregg@gmail.com>
2024-07-07 10:26:48 -0400

    Contact Service - Part 2

---
+++ contactapi/src/main/java/com/gii/contactapi/constant/Constant.java
@@ -0,0 +1,6 @@
+package com.gii.contactapi.constant;
+
+public class Constant {
+    public static final String PHOTO_DIRECTORY = System.getProperty("user.home")
+            + "/Downloads/uploads";
+}
--- contactapi/src/main/java/com/gii/contactapi/service/ContactService.java
+++ contactapi/src/main/java/com/gii/contactapi/service/ContactService.java
@@ -1,9 +1,18 @@
 package com.gii.contactapi.service;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gii.contactapi.domain.Contact;
 import com.gii.contactapi.repo.ContactRepo;
@@ -11,6 +20,9 @@ import com.gii.contactapi.repo.ContactRepo;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+import static com.gii.contactapi.constant.Constant.PHOTO_DIRECTORY;
 
 @Service
 @Slf4j
@@ -23,7 +35,7 @@ public class ContactService {
         return contactRepo.findAll(PageRequest.of(page, size, Sort.by("name")));
     }
 
-    public Contact geContact(String id) {
+    public Contact getContact(String id) {
         return contactRepo.findById(id).orElseThrow(() -> new RuntimeException("Contact not found"));
     }
 
@@ -34,4 +46,40 @@ public class ContactService {
     public void deleteContact(Contact contact) {
         contactRepo.delete(contact); // assignment to implement delete
     }
+
+    public String uploadPhoto(String id, MultipartFile file) {
+        Contact contact = getContact(id);
+        String photoUrl = null;
+        contact.setPhotoUrl(photoUrl);
+        contactRepo.save(contact);
+        return photoUrl;
+    }
+
+    private final Function<String, String> fileExtension = (filename) -> {
+        return Optional.of(filename)
+                .filter(name -> name.contains("."))
+                .map(name -> "." + name.substring(filename.lastIndexOf(".") + 1))
+                .orElse(".png");
+    };
+
+    private final BiFunction<String, MultipartFile, String> photoFunction = (id, image) -> {
+        String filename = id + fileExtension.apply(image.getOriginalFilename());
+
+        try {
+            Path fileStorageLocation = Paths.get(PHOTO_DIRECTORY).toAbsolutePath().normalize();
+            if (!Files.exists(fileStorageLocation)) {
+                Files.createDirectories(fileStorageLocation);
+            }
+
+            Files.copy(image.getInputStream(), fileStorageLocation.resolve(id + ".png"), REPLACE_EXISTING);
+
+            return ServletUriComponentsBuilder
+                    .fromCurrentContextPath()
+                    .path("/contacts/image/" + filename)
+                    .toUriString();
+        } catch (Exception exception) {
+            throw new RuntimeException("Unable to save image");
+        }
+    };
+
 }
